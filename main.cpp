7a1f2b1e5d9d900332f93b41ae85ce9836eaaf50
lucasktam <lucasktam@gmail.com>
2024-08-14 13:59:34 -0700

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <ncurses.h> 
 
-
 using namespace std;
 
 int main(int argc, char* argv[]){
-    int x,y;
-    x = y = 10;
     initscr();
-    int height = 10;
-    int width = 20;
-    WINDOW * win = newwin(height, width, y, x);
+    noecho();
+    cbreak();
+
+    int ymax, xmax;
+    getmaxyx(stdscr, ymax, xmax);
+
+    WINDOW * inputwin = newwin(3, xmax-12, ymax-5, 5);
+    box(inputwin, 0, 0);
     refresh();
-    box(win,0,0);
-    // wprintw(win, "Hello");
-    mvwprintw(win, 1, 1, "Hello");
-    wrefresh(win);
+    wrefresh(inputwin);
+
+    int c = wgetch(inputwin);
+    if (c == 'j'){
+        wprintw(inputwin, "You pressed J");
+        wrefresh(inputwin);
+    }
     getch();
+
     endwin();
+    return 0;
 }
